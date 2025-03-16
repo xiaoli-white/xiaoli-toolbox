07344dce407b66afd49421db92695ee20772c32c
XiaoLi <maxljh0216@yeah.net>
2025-03-16 17:02:12 +0800

    feat(Programmer): 改为string与utf8编码互转

--- src/components/programmer/AsciiString.vue
+++ src/components/programmer/AsciiString.vue
@@ -1,27 +1,29 @@
 <script lang="ts">
-  export default {
-    data() {
-      return {
-        ascii: "",
-        str: ""
-      }
+export default {
+  data() {
+    return {
+      ascii: "",
+      str: ""
+    }
+  },
+  methods: {
+    ascii2string() {
+      const textDecoder = new TextDecoder();
+      this.str = textDecoder.decode(new Uint8Array(this.ascii.split(",").map(c => Number(c))));
     },
-    methods: {
-      ascii2string() {
-        this.str = String.fromCharCode(...this.ascii.split(',').map(Number));
-      },
-      string2ascii() {
-        this.ascii = this.str.split('').map(char => char.charCodeAt(0).toString()).join(",");
-      }
+    string2ascii() {
+      const textEncoder = new TextEncoder();
+      this.ascii = textEncoder.encode(this.str).join(",");
     }
   }
+}
 </script>
 
 <template>
   <a-button @click="ascii2string">ASCII=>String</a-button>
   <a-button @click="string2ascii">String=>ASCII</a-button>
-  <a-input v-model:value="ascii" prefix="ASCII:" allow-clear />
-  <a-input v-model:value="str" prefix="String:" allow-clear />
+  <a-input v-model:value="ascii" prefix="ASCII:" allow-clear/>
+  <a-input v-model:value="str" prefix="String:" allow-clear/>
 </template>
 
 <style scoped>
