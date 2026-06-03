06b30246118955a2077ed7768033ea3e8690b4b1
xiaoli-white <xiaolitongxue-all@outlook.com>
2026-06-03 12:55:40 +0800

    feat(encrypt-decrypt): persist tab selection state to localStorage

--- src/EncryptAndDecrypt.vue
+++ src/EncryptAndDecrypt.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import Title from "./components/Title.vue";
 import RSA from "./components/encrypt-and-decrypt/RSA.vue";
 import AES128 from "./components/encrypt-and-decrypt/AES128.vue";
+
+const activeKey = ref(localStorage.getItem('main_tab_key') || "1");
+watch(activeKey, (val) => localStorage.setItem('main_tab_key', val));
 </script>
 
 <template>
   <Title/>
-  <a-tabs class="tabs" centered>
+  <a-tabs v-model:activeKey="activeKey" class="tabs" centered>
     <a-tab-pane key="1" tab="RSA" width="120px">
       <RSA/>
     </a-tab-pane>
--- src/components/encrypt-and-decrypt/RSA.vue
+++ src/components/encrypt-and-decrypt/RSA.vue
@@ -11,12 +11,19 @@ export default {
       encrypted: "",
       msg: "",
       ret: "",
-      result: ""
+      result: "",
+      subTabActiveKey: "1"
     }
   },
   mounted() {
     this.publicKey = localStorage.getItem('rsa_public_key') || "";
     this.privateKey = localStorage.getItem('rsa_private_key') || "";
+    this.subTabActiveKey = localStorage.getItem('rsa_sub_tab_key') || "1";
+  },
+  watch: {
+    subTabActiveKey(val) {
+      localStorage.setItem('rsa_sub_tab_key', val);
+    }
   },
   methods: {
     generateKeys() {
@@ -126,7 +133,7 @@ export default {
   <a-button @click="generateKeys">生成密钥对</a-button>
   <a-input v-model:value="publicKey" prefix="公钥:" allow-clear/>
   <a-input v-model:value="privateKey" prefix="私钥:" allow-clear/>
-  <a-tabs class="sub-tabs" centered>
+  <a-tabs v-model:activeKey="subTabActiveKey" class="sub-tabs" centered>
     <a-tab-pane tab="加解密" key="1">
       <a-button @click="encrypt">加密</a-button>
       <a-button @click="decrypt">解密</a-button>
